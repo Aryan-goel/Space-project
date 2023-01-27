0b6731b2b3cd22cee712234999e323a9efe3851b
91639 <goel.aryan2406@gmail.com>
2023-01-27 08:57:15 +0530

    [add]habitablePlanets

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,21 @@
-const http= require('http');
+const http = require('http');
 
-const app=require("./app")
- 
-const PORT=process.env.PORT || 8000;
+const app = require("./app")
+const { loadPlanetsData } = require('./models/planets.model')
 
-const server=http.createServer(app);
+const PORT = process.env.PORT || 8000;
 
-server.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}...`);
-})
+const server = http.createServer(app);
 
- 
+async function startServer() {
+
+    await loadPlanetsData();
+    
+    server.listen(PORT, () => {
+        console.log(`listening on port ${PORT}...`);
+    })
+}
+
+startServer();
+
+
