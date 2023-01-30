93601d8cc7f29755d03549ac4b990b7095c0698c
91639 <goel.aryan2406@gmail.com>
2023-01-30 13:11:16 +0530

    issue fixed

--- server/src/app.js
+++ server/src/app.js
@@ -12,5 +12,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','public')));
 
 app.use(planetsRouter);
+app.use('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','index.html'))
+})
 
 module.exports=app;
