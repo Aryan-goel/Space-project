5473759159bd8c99ae6c113051b3b1c546270168
91639 <goel.aryan2406@gmail.com>
2023-02-02 11:24:06 +0530

    [add]postRequest

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -7,11 +7,25 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
-    launch.launchDate = new Date(launch.launchDate);
-        addNewLaunch(launch)
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target){
+        return res.status(400).json({
+            error:"Missing required Launch Property"
+        })
 
+    }
+
+    launch.launchDate = new Date(launch.launchDate);
+
+    if(isNaN(launch.launchDate)){
+    return res.status (400).json({
+        error:"Invalid launch Date",
+    })
+    }
+        addNewLaunch(launch);
+        return res.status(201).json(launch);
 }
 
 module.exports = {
     httpGetAllLaunches,
+    httpAddNewLaunch,
 }
