432a4541aa1597c6045c6d89c8313b38c7aec980
91639 <goel.aryan2406@gmail.com>
2023-02-08 01:37:15 +0530

    [add]mongoDB

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+    flightNumber: {
+        type: Number,
+        required: true,
+    },
+
+    launchDate: {
+        type: Date,
+        required: true,
+    },
+    mission: {
+        type: String,
+        required: true,
+    },
+    rocket: {
+        type: String,
+        required: true,
+
+    },
+    target:{
+        type:mongoose.Types.ObjectId,
+        ref:'Planet'
+    }
+
+
+
+
+})
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const path= require('path');
+const path = require('path');
 const { parse } = require('csv-parse');
 
 const habitablePlanets = [];
@@ -10,7 +10,7 @@ function isHabitablePlanet(planet) {
 
 function loadPlanetsData() {
     return new Promise((resolve, reject) => {
-         fs.createReadStream(path.join(__dirname,"..","..","data","kepler_data.csv"))
+        fs.createReadStream(path.join(__dirname, "..", "..", "data", "kepler_data.csv"))
             .pipe(parse({
                 comment: '#',
                 columns: true,
@@ -33,12 +33,12 @@ function loadPlanetsData() {
     })
 }
 
-function getAllPlanets(){
-return habitablePlanets
+function getAllPlanets() {
+    return habitablePlanets
 }
 
 
 module.exports = {
     loadPlanetsData,
-   getAllPlanets
+    getAllPlanets
 }
